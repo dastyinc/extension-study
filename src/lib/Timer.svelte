c52a0cd8762bf754c1ccbfce4f60522a234b2f53
wagyu1001 <1001hermes@gmail.com>
2023-01-19 17:10:31 +0900

    fix: various

--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
+    import playSrc from '$static/Play.svg?url';
+    import pauseSrc from '$static/pause.svg?url';
 
-    let play = false;
+    export let StartTimer = () => null;
+    export let StopTimer = () => null;
+    export let play = false, hours, minutes, seconds;
+
+    $: {
+        if(play){
+            StartTimer();
+        }
+        else StopTimer();
+    }
 </script>
 
 <div>
-    <button on:click={() => play = !play}>{play ? "멈춤" : "시작"}</button>
-    
+    <img src={play ? playSrc : pauseSrc} on:click={() => play = !play}/>
+    <div class="time">{hours}</div>
+    <div class="time">{minutes}</div>
+    <div class="time">{seconds}</div>
 </div>
+
+<style lang="scss">
+    .time{
+        color: black;
+    }
+    
+    img:hover{
+        cursor: pointer;
+    }
+</style>
