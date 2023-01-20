4e0b7b620be482a43924dc886083af6099e947bd
03hgryan <03hgryan@gmail.com>
2023-01-19 21:28:14 -0800

    fix: componentize timer

--- src/App.svelte
+++ src/App.svelte
@@ -1,270 +1,303 @@
 <script lang="ts">
-    import {Box, Modal} from '@dastyinc/kit-panel';
-    import {slide} from "svelte/transition";
-    import {getContext, onMount} from "svelte";
+  import { Box, Modal } from "@dastyinc/kit-panel";
+  import { slide } from "svelte/transition";
+  import { getContext, onMount } from "svelte";
 
-    import checkboxSrc from "$static/checkbox.svg?url";
-    import checkedSrc from "$static/checked.svg?url";
-    import uncheckedSrc from "$static/unchecked.svg?url";
-    import closeSrc from "$static/close.svg?url";
-    import plusSrc from "$static/plus.svg?url";
-    import modalSrc from "$static/modal.png?url";
-    import Chart from '$lib/Chart.svelte';
-    import Ranking from '$lib/Ranking.svelte';
-    import Timer from '$lib/Timer.svelte';
-    import TodoList from '$lib/TodoList.svelte';
-    import { tweened } from 'svelte/motion';
+  import checkboxSrc from "$static/checkbox.svg?url";
+  import checkedSrc from "$static/checked.svg?url";
+  import uncheckedSrc from "$static/unchecked.svg?url";
+  import closeSrc from "$static/close.svg?url";
+  import plusSrc from "$static/plus.svg?url";
+  import modalSrc from "$static/modal.png?url";
+  import Chart from "$lib/Chart.svelte";
+  import Ranking from "$lib/Ranking.svelte";
+  import Timer from "$lib/Timer.svelte";
+  import TodoList from "$lib/TodoList.svelte";
+  import { tweened } from "svelte/motion";
 
-    const {api, ws, wsStore, throttle} = getContext('utils');
-    const {user_id, user_name} = getContext('account');
-    const channel = getContext('channel');
+  const { api, ws, wsStore, throttle } = getContext("utils");
+  const { user_id, user_name } = getContext("account");
+  const channel = getContext("channel");
 
-    export let showTodo = false;
-    export let showStudyModal = false;
+  export let showTodo = false;
+  export let showStudyModal = false;
 
-    let todos, todoList = [];
-    let goal = "", completed = false;
-    let _completed = 0;
-    let sendWs;
-    let time = 0;
-    const _time = 0;
-    let play = false;
+  let hours, minutes, seconds;
+  let todos,
+    todoList = [];
+  let goal = "",
+    completed = false;
+  let _completed = 0;
+  let sendWs;
+  const _time = 0;
+  let play = false;
 
-    $: hours = Math.floor(time / 3600)
-    $: minutes = Math.floor(time / 60);
-    $: seconds = Math.floor(time - hours * 3600 - minutes * 60)
+  let weeklyStudy = [
+    { x: "0", y: "3" },
+    { x: "1", y: "4" },
+    { x: "2", y: "4" },
+    { x: "3", y: "5" },
+    { x: "4", y: "8" },
+    { x: "5", y: "3" },
+    { x: "6", y: "9" },
+  ];
 
-    let weeklyStudy = [
-        {x : "0", y : "3"},
-        {x : "1", y : "4"},
-        {x : "2", y : "4"},
-        {x : "3", y : "5"},
-        {x : "4", y : "8"},
-        {x : "5", y : "3"},
-        {x : "6", y : "9"},
-    ]
-
-    $: {
-        _completed = 0;
-        todoList.forEach(todo => {
-            if (todo.completed) {
-                _completed += 1;
-            }
-        });
-    }
-
-    onMount(() => {
-        getTodoList();
-        getTime();
-        initTimeArr();
-    })
-
-    onMount(() => ws(channel).conn(({message, uid, send}) => {
-        sendWs = send;
-        wsStore(message, 'TODO_UPDATE').subscribe((msg) => updateTodoList(msg.id));
-    }))
-    $: sendTodoListUpdate = throttle(() => {
-        sendWs?.({type: 'TODO_UPDATE'});
+  $: {
+    _completed = 0;
+    todoList.forEach((todo) => {
+      if (todo.completed) {
+        _completed += 1;
+      }
     });
+  }
 
-    async function getTodoList() {
-        todos = await api(`/todoList/todo/user/${user_id}`)
-        todoList = todos.todos;
+  onMount(() => {
+    getTodoList();
+  });
+
+  onMount(() =>
+    ws(channel).conn(({ message, uid, send }) => {
+      sendWs = send;
+      wsStore(message, "TODO_UPDATE").subscribe((msg) =>
+        updateTodoList(msg.id)
+      );
+    })
+  );
+  $: sendTodoListUpdate = throttle(() => {
+    sendWs?.({ type: "TODO_UPDATE" });
+  });
+
+  async function getTodoList() {
+    todos = await api(`/todoList/todo/user/${user_id}`);
+    todoList = todos.todos;
+  }
+
+  async function deleteTodo(id) {
+    await api(`/todoList/todo/${id}`, {}, "DELETE");
+    getTodoList();
+    sendTodoListUpdate();
+  }
+
+  async function alterChecked(_goal) {
+    goal = _goal.goal;
+    completed = true;
+    deleteTodo(_goal.todo_id);
+    api("/todoList/todo", {
+      user_id,
+      user_name,
+      channel_id: channel,
+      goal,
+      completed,
+    })
+      .then(() => {})
+      .catch(({ error }) => {});
+    goal = "";
+    completed = false;
+    getTodoList();
+    sendTodoListUpdate();
+  }
+
+  function sendTodo() {
+    api("/todoList/todo", {
+      user_id,
+      user_name,
+      channel_id: channel,
+      goal,
+      completed,
+    })
+      .then(() => {
+        sendTodoListUpdate();
+      })
+      .catch(({ error }) => {});
+    getTodoList();
+    goal = "";
+  }
+
+  function onKeyPress(e) {
+    if (e.charCode === 13 && goal !== "") {
+      sendTodo();
     }
+  }
 
-    async function deleteTodo(id) {
-        await api(`/todoList/todo/${id}`, {}, 'DELETE')
-        getTodoList();
-        sendTodoListUpdate()
-    }
-
-    async function alterChecked(_goal) {
-        goal = _goal.goal;
-        completed = true;
-        deleteTodo(_goal.todo_id)
-        api('/todoList/todo', {user_id, user_name, channel_id: channel, goal, completed}).then(() => {
-        }).catch(({error}) => {
-        })
-        goal = "";
-        completed = false;
-        getTodoList();
-        sendTodoListUpdate()
-    }
-
-    function sendTodo() {
-        api('/todoList/todo', {user_id, user_name, channel_id: channel, goal, completed}).then(() => {
-            sendTodoListUpdate()
-        }).catch(({error}) => {
-        })
-        getTodoList();
-        goal = "";
-    }
-
-    function onKeyPress(e) {
-        if (e.charCode === 13 && goal !== "") {
-            sendTodo();
-        }
-    }
-
-    async function updateTodoList() {
-        await getTodoList()
-    }
-
-    let timeArr;
-    let timeObj;
-    let time_id;
-    let intervalId;
-
-    async function getTime(){
-        timeObj = await api(`/timer/time/${user_id}`);
-        timeArr = timeObj.time;
-    }
-
-    async function initTimeArr(){
-        if(timeArr?.length === 0){
-            await api(`/timer/time`, { user_id, user_name, channel_id: channel, time: _time });
-            console.log("put");
-        }
-    }
-
-    function startTimer(){
-        getTime();
-        time = timeArr[0].time;
-        time_id = timeArr[0].time_id;
-        clearInterval(intervalId);
-        intervalId = setInterval(() => {
-            if(play) time++;
-        }, 1000);
-    }
-
-    async function stopTimer(){
-        await api(`/timer/time/${time_id}`, 'DELETE');
-        await api(`/timer/time`, { user_id, user_name, channel_id: channel, time });
-    }
+  async function updateTodoList() {
+    await getTodoList();
+  }
 </script>
 
 <div style="display: flex; border-bottom: 1px solid rgba(255, 255, 255, 0.5);">
-    <div class="title">Study</div>
+  <div class="title">Study</div>
 </div>
 
 <div style="float: right; margin-top: -2.4rem; margin-right: 0.444rem">
-    <!-- <img src={modalSrc} class="expand" on:click={() => showStudyModal = !showStudyModal}/> -->
-    <img src={modalSrc} class="expand" on:click={() => alert("개발 중입니다")}/>
+  <img
+    src={modalSrc}
+    class="expand"
+    on:click={() => (showStudyModal = !showStudyModal)}
+  />
+  <!-- <img src={modalSrc} class="expand" on:click={() => alert("개발 중입니다")} /> -->
 </div>
 
-<Box background="#ffffff" style="margin-top: 1.219rem; padding: 0.938rem 1.25rem 0.812rem 1.25rem;">
-    <div style="display: flex; justify-content: space-between;">
-        <div class="top-div" on:click={() => showTodo = !showTodo}>
-            <div class="name" style="color: #ffffff;">Study</div>
-            <div class="status" style="color: black; margin-top: 0.625rem">TOEFL 공부 중</div>
-            <div style="display: flex; margin: 0.625rem 0 0.875rem 0">
-                <img src={checkboxSrc}/>
-                <div style="color: black; line-height: 1.5rem; margin-left: 0.312rem">{_completed} / {todoList.length}</div>
-            </div>
+<Box
+  background="#ffffff"
+  style="margin-top: 1.219rem; padding: 0.938rem 1.25rem 0.812rem 1.25rem;"
+>
+  <div style="display: flex; justify-content: space-between;">
+    <div class="top-div" on:click={() => (showTodo = !showTodo)}>
+      <div class="name" style="color: #ffffff;">Study</div>
+      <div class="status" style="color: black; margin-top: 0.625rem">
+        TOEFL 공부 중
+      </div>
+      <div style="display: flex; margin: 0.625rem 0 0.875rem 0">
+        <img src={checkboxSrc} />
+        <div style="color: black; line-height: 1.5rem; margin-left: 0.312rem">
+          {_completed} / {todoList.length}
         </div>
-        <div class="circle">
-            <div class="circle-text">{hours} : {minutes}</div>
-        </div>
+      </div>
     </div>
+    <div class="circle">
+      <div class="circle-text">{hours} : {minutes}</div>
+    </div>
+  </div>
 
-    {#if showTodo}
-        <div transition:slide>
-            <Box background="#28222d" shadow style="padding: 0.625rem; margin: 20px 0 0.438rem 0;">
-                <div class="name" style="background-color: white; color: #28222d">Todo-List</div>
-                {#each todoList as goal}
-                    {#if !goal.completed}
-                        <div class="goal">
-                            <img class="img" src={uncheckedSrc} on:click={alterChecked(goal)}/>
-                            <div style="margin-left: 0.625rem; width: 75%; font-size: 1rem;">{goal.goal}</div>
-                            <img class="close img" src={closeSrc} on:click|stopPropagation={deleteTodo(goal.todo_id)}/>
-                        </div>
-                    {/if}
-                {/each}
-                {#each todoList as goal}
-                    {#if goal.completed}
-                        <div class="goal">
-                            <img class="img" src={checkedSrc}/>
-                            <div style="margin-left: 0.625rem; width: 75%; font-size: 1rem;">{goal.goal}</div>
-                            <img class="close img" src={closeSrc} on:click|stopPropagation={deleteTodo(goal.todo_id)}/>
-                        </div>
-                    {/if}
-                {/each}
-                <div style="display: flex; margin-top: 0.625rem">
-                    <img src={plusSrc}/>
-                    <input bind:value={goal} on:keypress={onKeyPress} style="margin-left: 0.625rem;" placeholder="새로운 목표를 세워보세요!"/>
-                </div>
-            </Box>
+  {#if showTodo}
+    <div transition:slide>
+      <Box
+        background="#28222d"
+        shadow
+        style="padding: 0.625rem; margin: 20px 0 0.438rem 0;"
+      >
+        <div class="name" style="background-color: white; color: #28222d">
+          Todo-List
         </div>
-    {/if}
+        {#each todoList as goal}
+          {#if !goal.completed}
+            <div class="goal">
+              <img
+                class="img"
+                src={uncheckedSrc}
+                on:click={alterChecked(goal)}
+              />
+              <div style="margin-left: 0.625rem; width: 75%; font-size: 1rem;">
+                {goal.goal}
+              </div>
+              <img
+                class="close img"
+                src={closeSrc}
+                on:click|stopPropagation={deleteTodo(goal.todo_id)}
+              />
+            </div>
+          {/if}
+        {/each}
+        {#each todoList as goal}
+          {#if goal.completed}
+            <div class="goal">
+              <img class="img" src={checkedSrc} />
+              <div style="margin-left: 0.625rem; width: 75%; font-size: 1rem;">
+                {goal.goal}
+              </div>
+              <img
+                class="close img"
+                src={closeSrc}
+                on:click|stopPropagation={deleteTodo(goal.todo_id)}
+              />
+            </div>
+          {/if}
+        {/each}
+        <div style="display: flex; margin-top: 0.625rem">
+          <img src={plusSrc} />
+          <input
+            bind:value={goal}
+            on:keypress={onKeyPress}
+            style="margin-left: 0.625rem;"
+            placeholder="새로운 목표를 세워보세요!"
+          />
+        </div>
+      </Box>
+    </div>
+  {/if}
 </Box>
 
-<Modal bind:showModal={showStudyModal} style="width: fit-content; height: fit-content; padding: 1.375rem 2.375rem 2.375rem 2.375rem">
-    <div style="display: flex;">
-        <div class="name-bold">{user_name}</div>
-        <div class="name-normal" style="line-height: 2.2rem">의</div>
-    </div>
-    <div class="name-normal" style="margin-bottom: 1.438rem;">Study Dashboard</div>
-    
-    <div style="display: flex;">
-        <div style="margin-right: 1.375rem;">
-            <Box background="#f8f8f8" style="width: 18.813rem; height: 19.438rem; padding: 1.25rem">
-                <div class="name" style="color: #ffffff;">Today</div>
-                <Timer
-                    bind:hours={hours}
-                    bind:minutes={minutes}
-                    bind:seconds={seconds}
-                    bind:play={play}
-                    StartTimer={startTimer}
-                    StopTimer={stopTimer}
-                />
-            </Box>
+<Modal
+  bind:showModal={showStudyModal}
+  style="width: fit-content; height: fit-content; padding: 1.375rem 2.375rem 2.375rem 2.375rem"
+>
+  <div style="display: flex;">
+    <div class="name-bold">{user_name}</div>
+    <div class="name-normal" style="line-height: 2.2rem">의</div>
+  </div>
+  <div class="name-normal" style="margin-bottom: 1.438rem;">
+    Study Dashboard
+  </div>
 
-            <Box background="#28222d" style="width: 18.813rem; height: 12.438rem; margin-top: 1.375rem; padding: 1.25rem">
-                <div class="name" style="background-color: white; color: #28222d">Weekly</div>
-                <Chart data={weeklyStudy} style="margin-top: 1.25rem; height: 9.2rem"/>
-            </Box>
+  <div style="display: flex;">
+    <div style="margin-right: 1.375rem;">
+      <Box
+        background="#f8f8f8"
+        style="width: 18.813rem; height: 19.438rem; padding: 1.25rem"
+      >
+        <div class="name" style="color: #ffffff;">Today</div>
+        <Timer bind:hours bind:minutes bind:seconds bind:play />
+      </Box>
+
+      <Box
+        background="#28222d"
+        style="width: 18.813rem; height: 12.438rem; margin-top: 1.375rem; padding: 1.25rem"
+      >
+        <div class="name" style="background-color: white; color: #28222d">
+          Weekly
         </div>
-
-        <div>
-            <Box background="#28222d" style="width: 18.813rem; height: 15.938rem; padding: 1.25rem">
-                <div class="name" style="background-color: white; color: #28222d">Todo-List</div>
-                <TodoList 
-                    bind:todoList={todoList}
-                    bind:goal={goal}
-                    AlterChecked={alterChecked}
-                    DeleteTodo={deleteTodo}
-                    OnKeyPress={onKeyPress}
-                />
-            </Box>
-
-            <Box background="#f8f8f8" style="width: 18.813rem; height: 15.938rem; margin-top: 1.375rem; padding: 1.25rem">
-                <div class="name" style="color: #ffffff;">Ranking</div>
-                <Ranking/>
-            </Box>
-        </div>
+        <Chart data={weeklyStudy} style="margin-top: 1.25rem; height: 9.2rem" />
+      </Box>
     </div>
+
+    <div>
+      <Box
+        background="#28222d"
+        style="width: 18.813rem; height: 15.938rem; padding: 1.25rem"
+      >
+        <div class="name" style="background-color: white; color: #28222d">
+          Todo-List
+        </div>
+        <TodoList
+          bind:todoList
+          bind:goal
+          AlterChecked={alterChecked}
+          DeleteTodo={deleteTodo}
+          OnKeyPress={onKeyPress}
+        />
+      </Box>
+
+      <Box
+        background="#f8f8f8"
+        style="width: 18.813rem; height: 15.938rem; margin-top: 1.375rem; padding: 1.25rem"
+      >
+        <div class="name" style="color: #ffffff;">Ranking</div>
+        <Ranking />
+      </Box>
+    </div>
+  </div>
 </Modal>
 
 <style lang="scss">
-    .name-bold{
-        color:#28222d;
-        font-family: NotoSansKR;
-        font-size: 1.5rem;
-        font-weight: bold;
-    }
+  .name-bold {
+    color: #28222d;
+    font-family: NotoSansKR;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
 
-    .name-normal{
-        color:#28222d;
-        font-family: NotoSansKR;
-        font-size: 1.125rem;
-        font-weight: normal;
-    }
+  .name-normal {
+    color: #28222d;
+    font-family: NotoSansKR;
+    font-size: 1.125rem;
+    font-weight: normal;
+  }
 
-    .container{
-        margin-top: 0.4rem;
-        max-height: 12.5rem;
-        overflow-y: scroll;
-    }
+  .container {
+    margin-top: 0.4rem;
+    max-height: 12.5rem;
+    overflow-y: scroll;
+  }
 
   .title {
     margin: 0 16.813rem 0.594rem 0;
@@ -273,7 +306,7 @@
     color: #dce6f2;
   }
 
-  .expand:hover{
+  .expand:hover {
     cursor: pointer;
   }
 
@@ -348,7 +381,7 @@
     opacity: 0.5;
   }
 
-  .img:hover{
+  .img:hover {
     cursor: pointer;
   }
 </style>
--- src/lib/Timer.svelte
+++ src/lib/Timer.svelte
@@ -1,32 +1,81 @@
 <script lang="ts">
-    import playSrc from '$static/Play.svg?url';
-    import pauseSrc from '$static/pause.svg?url';
+  import playSrc from "$static/Play.svg?url";
+  import pauseSrc from "$static/pause.svg?url";
+  import { getContext, onMount } from "svelte";
 
-    export let StartTimer = () => null;
-    export let StopTimer = () => null;
-    export let play = false, hours, minutes, seconds;
+  export let play = false,
+    hours,
+    minutes,
+    seconds;
 
-    $: {
-        if(play){
-            StartTimer();
-        }
-        else StopTimer();
+  const { api } = getContext("utils");
+  const { user_id, user_name } = getContext("account");
+  const channel = getContext("channel");
+
+  let timeArr;
+  let timeObj;
+  let time_id;
+  let intervalId;
+  let time;
+  const _time = 0;
+
+  $: hours = Math.floor(time / 3600);
+  $: minutes = Math.floor(time / 60);
+  $: seconds = Math.floor(time - hours * 3600 - minutes * 60);
+
+  async function getTime() {
+    timeObj = await api(`/timer/time/${user_id}`);
+    timeArr = timeObj.time;
+    console.log(timeArr);
+  }
+
+  async function initTimeArr() {
+    if (timeArr?.length === 0) {
+      console.log("put");
+      await api(`/timer/time`, {
+        user_id,
+        user_name,
+        channel_id: channel,
+        time: _time,
+      });
     }
+  }
+
+  function startTimer() {
+    getTime();
+    time = timeArr[0].time;
+    time_id = timeArr[0].time_id;
+    clearInterval(intervalId);
+    intervalId = setInterval(() => {
+      if (play) time++;
+    }, 1000);
+  }
+
+  async function stopTimer() {
+    await api(`/timer/time/${time_id}`, "DELETE");
+    await api(`/timer/time`, { user_id, user_name, channel_id: channel, time });
+  }
+
+  $: {
+    if (play) {
+      startTimer();
+    } else stopTimer();
+  }
 </script>
 
 <div>
-    <img src={play ? playSrc : pauseSrc} on:click={() => play = !play}/>
-    <div class="time">{hours}</div>
-    <div class="time">{minutes}</div>
-    <div class="time">{seconds}</div>
+  <img src={play ? playSrc : pauseSrc} on:click={() => (play = !play)} />
+  <div class="time">{hours}</div>
+  <div class="time">{minutes}</div>
+  <div class="time">{seconds}</div>
 </div>
 
 <style lang="scss">
-    .time{
-        color: black;
-    }
-    
-    img:hover{
-        cursor: pointer;
-    }
+  .time {
+    color: black;
+  }
+
+  img:hover {
+    cursor: pointer;
+  }
 </style>
